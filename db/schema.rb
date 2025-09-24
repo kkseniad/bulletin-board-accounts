dde2d9a78ca8edce44f9cd05f3576485dd446679
Kseniia Kormalova <kormalovak@gmail.com>
2025-09-24 22:36:33 +0000

    added user_id column to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_24_223437) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_24_223606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_24_223437) do
     t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "solid_cable_messages", force: :cascade do |t|
